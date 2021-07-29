8c204a84efacab2aeac3d3b76d83eef38b1eb66d
IkhtiyorDev <ikhtiyordev@gmail.com>
2021-07-29 08:10:12 +0500

    commit

---
+++ src/test/java/dev/ikhtiyor/springbootmultilang/SpringBootMultiLangApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ikhtiyor.springbootmultilang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultiLangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
